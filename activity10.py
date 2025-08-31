a359b304ed8c78bfa7610e0d8b24ae96d2837194
Sandra Vanesa R. Del Valle <sd6258039@gmail.com>
2025-08-31 23:32:58 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "sandra123"
+password = "svvl123"
+
+urname = input("Enter usernam ---> ")
+pword = input("nter password ---> ")
+
+if (urname == username) and (pword == password):
+	 print("ACCESS GRANTED")
+else:
+	 print("ACCESS DENIED")
