c9fd563de76879707eff5560b917fc27825bff5a
Sandra Vanesa R. Del Valle <sd6258039@gmail.com>
2025-09-17 17:53:07 +0800

    Add files via upload

---
+++ code_challenge9.py
@@ -0,0 +1,10 @@
+# 🦜 PARROT SIMULATOR
+
+phrase = input("What do you want your parrot to say? ")
+
+times = int(input("How many times should the parrot squawk it? "))
+
+print("Listen to your parrot:")
+
+for i in range(times):
+    print("🦜 Squawk!", phrase)
