3082789f89aff68676e0d58aa932dca3576b68e6
Sandra Vanesa R. Del Valle <sd6258039@gmail.com>
2025-09-17 17:42:15 +0800

    Add files via upload

---
+++ code_challenge7.py
@@ -0,0 +1,10 @@
+# COUNTDOWN TIMER SIMULATOR
+
+start = int(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+
+for i in range(start, 0, -1):
+    print(i)
+
+print("Liftoff!")
